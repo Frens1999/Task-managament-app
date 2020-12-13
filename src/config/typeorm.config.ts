6ceb7c6ccb9f2bae401061625c42b5c5d7b676d9
Frens <frenskadiasi@gmail.com>
2020-12-13 23:20:14 +0100

    ready for prod

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,12 +2,12 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import * as config from 'config';
 const dbConfig = config.get('db');
 export const typeOrmConfig: TypeOrmModuleOptions = {
+    url: process.env.DATABASE_URL,
     type: dbConfig.type,
     host: process.env.RDS_HOSTNAME || dbConfig.host,
     port: process.env.RDS_PORT || dbConfig.port,
     username: process.env.RDS_USERNAME || dbConfig.username,
     password: process.env.RDS_PASSWORD || dbConfig.password,
-    database: process.env.RDS_DB_NAME || dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
 }
